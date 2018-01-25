bee3d98764b1df249ee9e7d74958793511e97e6e
Tetiana <33543309+kryvoshapka@users.noreply.github.com>
2018-01-25 21:54:29 +0200

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -22,4 +22,45 @@ $(window).ready(function() {
             $('nav .phone-number').prependTo('header .phone-block');
         }
     });
+
+
+    $(".get-info").click(function () {
+        $(".modal").fadeIn(400).css("display","flex");
+    });
+
+    $(".modal").click(function (event) {
+        var clickedElemnt = event.target;
+        if(!$(clickedElemnt).closest(".modal-inner").length) {
+            $(".modal").fadeOut(400);
+        }
+        if($(clickedElemnt).hasClass(".fa-times")) {
+            $('.modal').fadeOut(400);
+        }
+    });
+
+
+    $(".order-call").click(function () {
+        $(".modal-2").fadeIn(400).css("display","flex");
+    });
+
+    $(".modal-2").click(function (event) {
+        var clickedElemnt = event.target;
+        if(!$(clickedElemnt).closest(".modal-inner").length) {
+            $(".modal-2").fadeOut(400);
+        }
+        if($(clickedElemnt).hasClass(".fa-times")) {
+            $('.modal-2').fadeOut(400);
+        }
+    });
+
+
+    $('a[href^="#"]').click(function(){
+
+        var target = $(this).attr('href');
+        $('html, body').animate({scrollTop: $(target).offset().top-100}, 1000);
+
+        return false;
+
+    });
+
 });
